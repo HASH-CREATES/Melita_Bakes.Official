df66895db86287330ca44247b5dc29d8d0cff0a7
HASH-CREATES <ha9931550@gmail.com>
2025-06-26 23:12:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'  // <-- ADD THIS
 import App from './App'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>  {/* <-- WRAP APP WITH THIS */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
