d7d660c3598b6bb643721973bb5db4412c3e01ef
HASH-CREATES <ha9931550@gmail.com>
2025-06-29 00:38:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { supabase } from './supabaseClient';
 import React, { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
 import './App.css';
-import { createClient } from '@supabase/supabase-js';
 import { useNavigate } from 'react-router-dom';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 function App() {
   const navigate = useNavigate();
