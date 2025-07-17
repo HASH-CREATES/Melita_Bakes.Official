e4e50baa85859e23f3812cb4de764d0ec857887b
HASH-CREATES <ha9931550@gmail.com>
2025-07-17 16:46:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  define: {
-    __APP_ENV__: JSON.stringify(process.env.NODE_ENV),
-  },
+  server: { port: 3000 },
+  build: { outDir: 'dist' },
+  define: { __APP_ENV__: JSON.stringify(process.env.NODE_ENV) }
 });
