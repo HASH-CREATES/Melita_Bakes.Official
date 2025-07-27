9a2be3b083b6f47f7b5da85602b57ce9edd03692
HASH-CREATES <ha9931550@gmail.com>
2025-07-27 23:41:54 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl  = 'https://cyknvmjspyblbdbdcuei.supabase.co';
+
+
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5a252bWpzcHlibGJkYmRjdWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3ODUwODksImV4cCI6MjA2NjM2MTA4OX0.cpbgOl6S9kKdXMs3HVIeyggItNaFZVyAfQ3BH9u0qh0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
