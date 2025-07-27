6f47d16be8e7129a3e5f2040e0110d0ccfad3876
HASH-CREATES <ha9931550@gmail.com>
2025-07-28 00:27:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Environment from 'vite-plugin-environment';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Environment(['VITE_SUPABASE_URL', 'VITE_SUPABASE_ANON_KEY'])
+  ],
   base: '/',
   server: { port: 3000 },
-  build: { outDir: 'dist' },
-  define: { __APP_ENV__: JSON.stringify(process.env.NODE_ENV) }
+  build: { outDir: 'dist' }
 });
